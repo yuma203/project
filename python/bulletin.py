805d7e1995287227db3ad9b1d07b7ed9536749d8
yuma203 <yuma131014@gmail.com>
2020-11-19 09:26:08 +0900

    名前＆時間の表示

--- python/bulletin.py
+++ python/bulletin.py
@@ -1,5 +1,6 @@
 from bottle import route, run, template, request
 import fileUtil
+import datetime
 
 @route('/bulletin')
 def bulletin():
@@ -10,9 +11,20 @@ def bulletin():
 
 @route('/bulletin', method='POST')
 def do_hello():
+    name = request.forms.name
     txt = request.forms.message
+    timeNum = ""
+
+    if len(name) == 0:
+        name = "名無しさん"
+
+    for time in range(16):
+        timeNum += str(datetime.datetime.now())[time]
+
     currentList = fileUtil.readFile("sample.txt")
-    newList = currentList + txt + '<br>'
+    newList = currentList
+    if len(txt) != 0:
+        newList = currentList + "NAME: " + name + '<br>' + "TIME: " + "{}".format(timeNum) + "<br>" + "TEXT: " + txt + "<br><br>"
 
     fileUtil.writeFile("sample.txt", newList)
 
